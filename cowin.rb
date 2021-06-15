e0334797d5b6ee4695d6235d0e0324dba18f9b77
Raghu Boosetty <raghu.boosetty@copart.com>
2021-06-15 10:44:15 +0530

    fix NPE and re-factor

--- cowin.rb
+++ cowin.rb
@@ -4,45 +4,40 @@ require 'awesome_print'
 require 'pry'
 
 class CowinVaccine
-  attr_reader :state_id, :dose, :age, :vaccines, :date_range
-  attr_accessor :availability
+  DISTRICT_LIST_URL = "https://cdn-api.co-vin.in/api/v2/admin/location/districts/state_id"
+  VACCINE_BY_DISTRICT_URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict"
+
+  attr_reader :state_id, :dose, :age, :vaccines, :date_range, :districts, :availability
 
   def initialize(opts={})
-    @state_id = opts[:state_id] || 32
+    @state_id = (opts[:state_id] || 32).to_s
     @dose = opts[:dose] || 1
     @age = opts[:age] || 18
     @vaccines = opts[:vaccines] || ['COVISHIELD', 'COVAXIN']
-    @date_range = (Date.today..Date.today + 2).map{ |d| d.strftime('%d-%m-%Y') }
+    @districts = api_get(DISTRICT_LIST_URL.gsub("state_id", state_id)).dig("districts")
     @availability = {}
-  end
-
-  def districts
-    @districts ||= api_get("https://cdn-api.co-vin.in/api/v2/admin/location/districts/#{state_id}").dig("districts")
   end
 
   def fetch_vaccines
     districts.each_with_index do |district, district_index|
-      centers = api_get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=#{district['district_id']}&date=#{Date.today.strftime('%d-%m-%Y')}").dig('centers')
+      centers = api_get("#{VACCINE_BY_DISTRICT_URL}?district_id=#{district['district_id']}&date=#{Date.today.strftime('%d-%m-%Y')}").dig('centers')
       centers.each_with_index do |center, center_index|
         availability[district['district_name']] = {} unless availability[district['district_name']]
         center['sessions']&.each do |session|
-          # if center['name'] == "Share Medical Care Mediciti"
-          #   binding.pry
-          # end
           availability[district['district_name']][center['name']] = [] unless availability[district['district_name']][center['name']]
           if session["available_capacity_dose#{dose}"].positive? && vaccines.any? { |v| session['vaccine'] == v } && session['min_age_limit'] == age
             availability[district['district_name']][center['name']] << { 
-              "#{session['date']}" => "D#{dose} #{session['min_age_limit']}+ #{session['vaccine']} #{center['fee_type']} #{center['vaccine_fees'].find {|fee| fee['vaccine'] == session['vaccine'] }.dig('fee')} - #{session['available_capacity_dose' + dose.to_s]}"
+              "#{session['date']}" => "D#{dose} #{session['min_age_limit']}+ #{session['vaccine']} #{center['fee_type']} #{center['vaccine_fees']&.find {|fee| fee['vaccine'] == session['vaccine'] }&.dig('fee')} - #{session['available_capacity_dose' + dose.to_s]}"
             }
           end
         end
         availability[district['district_name']].delete(center['name']) if availability[district['district_name']][center['name']].empty?
       end
-      availability.delete(district['district_name']) if availability[district['district_name']].empty?
+      availability.delete(district['district_name']) if availability[district['district_name']] && availability[district['district_name']].empty?
     end
   end
 
-
+private
   def api_get(url)
     headers = {
       'Content-Type': 'application/json', 
@@ -57,6 +52,6 @@ class CowinVaccine
   end
 end
 
-cv = CowinVaccine.new(state_id: 32, dose: 2, age: 18, vaccines: ['COVAXIN'])
+cv = CowinVaccine.new(state_id: 21, dose: 1, age: 18, vaccines: ['COVAXIN'])
 cv.fetch_vaccines
 ap cv.availability
